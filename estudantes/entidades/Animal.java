5beb96ec730770e537ed2409bdfcc0d69602d1c2
Ana Caroline Brandão Costa <costaanab1998@gmail.com>
2023-10-14 22:55:12 -0300

    AlteraçãoInicialAna

--- estudantes/entidades/Animal.java
+++ estudantes/entidades/Animal.java
@@ -1,23 +1,8 @@
 package estudantes.entidades;
 
+import java.util.Objects;
 
-/**
- * Classe que define um animal da arca.
- * <br><br>
- * Essa classe pode ser estendida. Adicionalmente, ela deve:
- * <br><br>
- * 1) ter um construtor completo,<br>  (DONE)
- * 2) implementar métodos de acesso (getters), sendo que métodos de modificação
- * (setters) NÃO devem ser implementados,<br>
- * 3) sobre-escrever os métodos toString, equals e hashCode,<br>
- * 4) deve implementar o método <i>aumentaEspera</i> conforme indicado na
- * documentação.
- * <br><br>
- * <strong>Não devem ser criados métodos adicionais nessa classe.</strong>
- * 
- * @author Jean Cheiran
- * @version 1.0
- */
+
 public class Animal {
     
     /**
@@ -37,12 +22,13 @@ public class Animal {
      * Construtor do animal.
      * Todos os atributos são passados por parâmetro, exceto o tempo de espera
      * que sempre começa em 0.
-     * @param id
-     * @param nome
-     * @param especie
-     * @param andarDesejado
-     * @param peso
-     * @param temperaturaIdeal 
+     * 
+     * @param id              O número de identificação do animal.
+     * @param nome            O nome do animal.
+     * @param especie         A espécie do animal.
+     * @param andarDesejado   O andar para o qual o animal deseja ir (0 é o térreo).
+     * @param peso            O peso do animal em quilos.
+     * @param temperaturaIdeal A temperatura ideal do animal em graus Celsius.
      */
     public Animal(int id, String nome, String especie, int andarDesejado, 
             int peso, int temperaturaIdeal){
@@ -53,72 +39,84 @@ public class Animal {
         this.peso = peso;
         this.temperaturaIdeal = temperaturaIdeal;
         this.tempoDeEspera = 0;
-  }
+    }
 
     /**
-     * Retorna o número de identificaçao do animal.
-     * O valor é gerado aleatoriamente e está entre 0 e 999999.
-     * @return número da identificação do animal
+     * Retorna o número de identificação do animal.
+     * 
+     * @return O número de identificação do animal.
      */
     public int getId(){
         return id;
     }
+
     /**
      * Retorna o nome do animal.
-     * @return nome do animal
+     * 
+     * @return O nome do animal.
      */
     public String getNome(){
         return nome;
     }
-      /**
+
+    /**
      * Retorna a espécie do animal.
-     * @return espécie do animal
+     * 
+     * @return A espécie do animal.
      */
     public String getEspecie(){
         return especie;
     }
-       /**
+
+    /**
      * Retorna o andar que o animal deseja ir.
-     * @return andar desejado pelo animal
+     * 
+     * @return O andar desejado pelo animal.
      */ 
     public int getAndarDesejado(){
         return andarDesejado;
     }
+
     /**
      * Retorna o peso do animal.
-     * O peso é gerado aleatoriamente e está entre 1 e 1.000 quilos.
-     * @return peso do animal em quilos
+     * 
+     * @return O peso do animal em quilos.
      */
     public int getPeso(){
         return peso;
     }
+
     /**
      * Retorna a temperatura ideal do animal.
-     * @return temperatura ideal do animal em graus Celsius
+     * 
+     * @return A temperatura ideal do animal em graus Celsius.
      */
     public int getTemperaturaIdeal(){
         return temperaturaIdeal;
     }
-     /**
+
+    /**
      * Retorna o tempo de espera do animal.
-     * @return tempo de espera do animal em segundos (ciclos de espera)
+     * 
+     * @return O tempo de espera do animal em segundos (ciclos de espera).
      */
     public int getTempoDeEspera(){
         return tempoDeEspera;
     }
+
     /**
      * Retorna uma representação em String do animal.
      * A representação é composta por todos os atributos do animal, separados
      * por vírgula e espaço, e entre colchetes.
-     * @return representação em String do animal
+     * 
+     * @return Uma representação em String do animal.
      */
-    
     public String toString(){
         return "["+id+", "+nome+", "+especie+", "+andarDesejado+", "+peso+", "+temperaturaIdeal+", "+tempoDeEspera+"]";
     }
- 
+
     /**
-     * Aumenta o tempo de espera um animal na fila quando passa um ciclo.
+     * Aumenta o tempo de espera de um animal na fila quando passa um ciclo.
      * Esse é o tempo de espera na fila de um andar para embarcar no elevador.
      * Um ciclo ocorre sempre que o método de simular vida na arca é invocado.
      * Esse método não deve ser chamado fora da classe Arca.
@@ -126,8 +124,8 @@ public class Animal {
      * A implementação desse método deve comparar a paciência do animal com
      * o tempo de espera depois de incrementado. Se ela for menor, uma exceção
      * deve ser lançada.
-     * <br><br>
-     * @throws RuntimeException se o animal está esperando a mais tempo que a paciência
+     * 
+     * @throws RuntimeException Se o animal está esperando a mais tempo que a paciência.
      * @see professor.entidades.Arca#simularVida
      */
     public void aumentaEspera(){
@@ -135,6 +133,34 @@ public class Animal {
         if(tempoDeEspera > PACIENCIA_MAXIMA){
             throw new RuntimeException("Animal esperando a mais tempo que a paciência");
         }
-        
+    }
+
+    /**
+     * Sobrescreve o método equals para comparar dois objetos Animal.
+     * Dois animais são considerados iguais se tiverem o mesmo ID.
+     * 
+     * @param outro O objeto a ser comparado com o objeto atual.
+     * @return true se os objetos são iguais, false caso contrário.
+     */
+    @Override
+    public boolean equals(Object outro) {
+        if (this == outro) {
+            return true;
+        }
+        if (outro == null || getClass() != outro.getClass()) {
+            return false;
+        }
+        Animal outroAnimal = (Animal) outro;
+        return id == outroAnimal.id;
+    }
+
+    /**
+     * Sobrescreve o método hashCode para gerar um código de hash com base no ID.
+     * 
+     * @return O código de hash gerado.
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
