c00a8b992de2597b2548027b85b581689fff3041
Ana Caroline Brandão Costa <costaanab1998@gmail.com>
2023-10-16 12:48:30 -0300

    VersãoFinalProf

--- estudantes/entidades/Ascensorista.java
+++ estudantes/entidades/Ascensorista.java
@@ -1,154 +1,54 @@
 package estudantes.entidades;
 
+
 import professor.entidades.Andar;
 import professor.entidades.Elevador;
-import professor.entidades.Arca;
-import java.util.List;
 
+
+/**
+ * Classe que traz a lógica do algoritmo de uso do elevador.
+ * <br><br>
+ * Você pode incluir novos atributos e métodos nessa classe para criar
+ * lógicas mais complexas para o gerenciamento do uso do elevador, mas esses
+ * <strong>atributos e métodos devem ser todos privados</strong>. O único
+ * método público deve ser "agir".
+ * 
+ * @author Jean Cheiran
+ * @version 1.1
+ */
 public class Ascensorista {
-
-    public Ascensorista() {
-        
-    }
-
+    
     /**
-     * Realiza ações de controle no elevador com base nas condições do andar.
-     *
-     * @param elevador O elevador a ser controlado.
-     * @param andar    O andar com informações para controle.
+     * Construtor padrão de Ascensorista.
+     * Esse construtor sem parâmetros que será usado pela Arca. Embora a
+     * assinatura do construtor não deva ser mudada, o código interno pode
+     * ser alterado conforme a necessidade.
      */
-    public void agir(Elevador elevador, Andar andar) {
-        // Controle da temperatura do ar-condicionado
-        controlarTemperatura(elevador, andar);
-
-        // Controle do alagamento do elevador
-        controlarAlagamento(elevador, andar);
-
-        // Controle do peso máximo
-        controlarPeso(elevador);
-
-        // Controle de embarque e desembarque
-        controlarEmbarqueDesembarque(elevador, andar);
-
-        // Atualizar o estado do elevador (subir ou descer)
-        controlarMovimentoElevador(elevador, andar);
-    }
-
-    private void controlarTemperatura(Elevador elevador, Andar andar) {
-        try {
-            double temperaturaDesejada = andar.getTemperaturaDoArCondicionado();
-            int temperaturaAtual = elevador.getTemperaturaDoArCondicionado();
-            if (temperaturaAtual < temperaturaDesejada - 15) {
-                elevador.setTemperaturaDoArCondicionado((int) (temperaturaDesejada - 15));
-                System.out.println("A temperatura do ar-condicionado foi ajustada para esfriar o elevador.");
-            } else if (temperaturaAtual > temperaturaDesejada + 15) {
-                elevador.setTemperaturaDoArCondicionado((int) (temperaturaDesejada + 15));
-                System.out.println("A temperatura do ar-condicionado foi ajustada para aquecer o elevador.");
-            } else {
-                System.out.println("A temperatura do ar-condicionado está dentro da faixa aceitável.");
-            }
-        } catch (RuntimeException e) {
-            System.err.println("Erro ao ajustar a temperatura do ar-condicionado: " + e.getMessage());
-        }
+    public Ascensorista(){
+        /* TODO: codificar */
     }
     
-    private void controlarAlagamento(Elevador elevador, Andar andar) {
-        try {
-            boolean nivelAgua = andar.getNivelAgua() > 0;
-            if (nivelAgua && !elevador.isCheioDeAgua()) {
-                elevador.encher();
-                System.out.println("O elevador foi enchido de água devido ao nível de água alto no andar.");
-            } else if (!nivelAgua && elevador.isCheioDeAgua()) {
-                elevador.drenar();
-                System.out.println("A água foi drenada do elevador devido à diminuição do nível de água no andar.");
-            } else {
-                System.out.println("O elevador não está cheio de água, e o nível de água no andar é aceitável.");
-            }
-        } catch (RuntimeException e) {
-            System.err.println("Erro ao controlar o alagamento do elevador: " + e.getMessage());
-        }
+    /**
+     * Executa a lógica de controle do elevador e dos animais.
+     * Esse método é o único método de controle invocado durante a simulação 
+     * de vida da arca.
+     * <br><br>
+     * Aqui podem ser feitas todas as verificações sobre os animais do elevador
+     * e da fila de animais que estão esperando no andar. A partir desses
+     * estados, você pode movimentar animais para dentro e para fora do
+     * elevador usando os métodos "desembarcar" e "embarcar" por exemplo.
+     * O estado do elevador também é importante para acionar os comandos do 
+     * elevador como "drenar", "encher", "subir" e "descer".
+     * <br><br>
+     * O método de subir ou descer só deve ser acionado uma vez durante a 
+     * invocação desse método. Por exemplo, se o método subir foi invocado em
+     * algum momento desse método, ele não pode ser invocado de novo e nem o
+     * método de descer pode ser invocado.
+     * @param elevador o elevador controlado pelo ascensorista
+     * @param andar o andar no qual o elevador está parado
+     */
+    public void agir(Elevador elevador, Andar andar){
+        /* TODO: codificar */
     }
     
-
-    private void controlarPeso(Elevador elevador) {
-        try {
-            double pesoMaximo = elevador.getLimiteDePeso();
-            double pesoAtual = elevador.calcularPesoAnimais();
-            if (pesoAtual > pesoMaximo) {
-                // Remover animais com base no peso
-                List<Animal> animaisNoElevador = elevador.checarAnimaisNoElevador();
-                double pesoRemovido = 0;
-                int i = 0;
-                while (pesoAtual > pesoMaximo && i < animaisNoElevador.size()) {
-                    Animal animalARemover = animaisNoElevador.get(i);
-                    if (pesoRemovido + animalARemover.getPeso() <= pesoAtual - pesoMaximo) {
-                        elevador.desembarcar(animalARemover, andar);
-                        pesoRemovido += animalARemover.getPeso();
-                    }
-                    i++;
-                }
-                System.out.println("Animais removidos para aliviar o peso no elevador.");
-            } else {
-                System.out.println("O peso no elevador está dentro do limite aceitável.");
-            }
-        } catch (IllegalArgumentException e) {
-            System.err.println("Erro ao controlar o peso do elevador: " + e.getMessage());
-        }
-    }
-
-    private void controlarEmbarqueDesembarque(Elevador elevador, Andar andar) {
-        controlarEmbarque(elevador, andar);
-        controlarDesembarque(elevador, andar);
-    }
-
-    private void controlarEmbarque(Elevador elevador, Andar andar) {
-        List<Animal> animaisNoAndar = andar.getAnimaisNoAndar();
-        for (Animal animal : animaisNoAndar) {
-            try {
-                if (elevador.podeEmbarcar(animal) && !elevador.isCheio()) {
-                    elevador.embarcar(animal);
-                    System.out.println("Um animal embarcou no elevador.");
-                }
-            } catch (IllegalStateException e) {
-                System.err.println("Erro ao embarcar animal no elevador: " + e.getMessage());
-            }
-        }
-    }
-
-    private void controlarDesembarque(Elevador elevador, Andar andar) {
-        List<Animal> animaisNoElevador = elevador.checarAnimaisNoElevador();
-        for (Animal animal : animaisNoElevador) {
-            if (andar.desembarcar(animal)) {
-                try {
-                    elevador.desembarcar(animal, andar);
-                    System.out.println("Um animal desembarcou do elevador.");
-                } catch (IllegalStateException e) {
-                    System.err.println("Erro ao desembarcar animal do elevador: " + e.getMessage());
-                }
-            }
-        }
-    }
-
-    private void controlarMovimentoElevador(Elevador elevador, Andar andar) {
-        int andarAtual = elevador.getAndarAtual();
-        int destinoAndar = andar.getNumeroAndar();
-
-        if (andarAtual < destinoAndar) {
-            if (elevador.podeSubir()) {
-                elevador.subir();
-                System.out.println("O elevador está subindo para o andar de destino.");
-            } else {
-                System.out.println("O elevador já está no andar mais alto possível.");
-            }
-        } else if (andarAtual > destinoAndar) {
-            if (elevador.podeDescer()) {
-                elevador.descer();
-                System.out.println("O elevador está descendo para o andar de destino.");
-            } else {
-                System.out.println("O elevador já está no andar mais baixo possível.");
-            }
-        } else {
-            System.out.println("O elevador já está no andar de destino.");
-        }
-    }
 }
