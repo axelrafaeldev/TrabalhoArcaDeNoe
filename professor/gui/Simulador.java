e3f4e8739c7fb9f6b677bc4d18c519d129846a06
Ana Caroline Brandão Costa <costaanab1998@gmail.com>
2023-10-16 12:47:08 -0300

    VersaoFinalProf

--- professor/gui/Simulador.java
+++ professor/gui/Simulador.java
@@ -10,7 +10,7 @@ import java.awt.Color;
  * <strong>Não mexa aqui!!!</strong>
  * 
  * @author Jean Cheiran
- * @version 1.0
+ * @version 1.1
  */
 public class Simulador extends javax.swing.JFrame {
 
