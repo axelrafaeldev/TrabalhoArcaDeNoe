bc80621e6ba3c2eaa73de1b89481b622316edd5f
Ana Caroline Brandão Costa <costaanab1998@gmail.com>
2023-10-16 15:17:18 -0300

    AtendeOsRequisitos

--- estudantes/entidades/Animal.java
+++ estudantes/entidades/Animal.java
@@ -63,10 +63,6 @@ public class Animal {
         return temperaturaIdeal;
     }
 
-    public int getTempoDeEspera() {
-        return tempoDeEspera;
-    }
-
     public void resetTempoEspera() {
         tempoDeEspera = 0;
     }
